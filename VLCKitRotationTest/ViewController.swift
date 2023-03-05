0bea8e97624d21f58e163b6573950051d2e81b5e
Marcel Müller <marcel-mueller@gmx.de>
2023-03-05 17:00:54 +0100

    Add timer to show device orientation

--- VLCKitRotationTest/ViewController.swift
+++ VLCKitRotationTest/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+            self.updateLabel()
+        }
     }
 
+    private func updateLabel() {
+        var text = ""
 
+        switch UIDevice.current.orientation {
+        case .portrait:
+            text = "Portrait"
+        case .portraitUpsideDown:
+            text = "PortraitUpsideDown"
+        case .landscapeLeft:
+            text = "LandscapeLeft"
+        case .landscapeRight:
+            text = "LandscapeRight"
+        default:
+            text = "Unknown"
+        }
+
+        helloLabel.text = text
+    }
+
+    @IBAction func openDialogTouchUpInside(_ sender: Any) {
+        let vc = UIViewController()
+        self.present(vc, animated: true, completion: nil)
+    }
 }
 
