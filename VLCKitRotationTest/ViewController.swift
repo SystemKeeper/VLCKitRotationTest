2dd4587edd27805bad9deec1819c421a3ea8f164
Marcel Müller <marcel-mueller@gmx.de>
2023-03-05 17:17:26 +0100

    Add motion information

--- VLCKitRotationTest/ViewController.swift
+++ VLCKitRotationTest/ViewController.swift
@@ -6,16 +6,38 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var orientationLabel: UILabel!
+    @IBOutlet weak var motionLabel: UILabel!
+
+    private var motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
             self.updateLabel()
+        }
+
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 0.1
+
+            let queue = OperationQueue()
+            motionManager.startDeviceMotionUpdates(to: queue, withHandler: { motion, error in
+                if let attitude = motion?.attitude {
+                    DispatchQueue.main.async {
+                        var yaw = String(format: "%.0f", attitude.yaw * 180.0 / Double.pi)
+                        var roll = String(format: "%.0f", attitude.roll * 180.0 / Double.pi)
+                        var pitch = String(format: "%.0f", attitude.pitch * 180.0 / Double.pi)
+
+                        self.motionLabel.text = "Yaw: \(yaw) | Roll: \(roll) | Ptich: \(pitch)"
+                    }
+                }
+
+            })
         }
     }
 
@@ -35,7 +57,7 @@ class ViewController: UIViewController {
             text = "Unknown"
         }
 
-        helloLabel.text = text
+        orientationLabel.text = text
     }
 
     @IBAction func openDialogTouchUpInside(_ sender: Any) {
